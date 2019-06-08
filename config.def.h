3ae7e2f100cecf5c4b900e553f889c79edb191e3
reroche <rroche@andrew.cmu.edu>
2019-06-08 13:31:05 -0400

    config

--- config.def.h
+++ config.def.h
@@ -63,8 +63,8 @@ static Key keys[] = {
  { MODKEY,              XK_b,                togglebar,      {0} },
  { MODKEY,              XK_j,                focusstack,     {.i = +1 } },
  { MODKEY,              XK_k,                focusstack,     {.i = -1 } },
- { MODKEY|ShiftMask,    XK_j,                movestack,      {.i = +1 } },
- { MODKEY|ShiftMask,    XK_k,                movestack,      {.i = -1 } },
+ /* { MODKEY|ShiftMask,    XK_j,                movestack,      {.i = +1 } }, */
+ /* { MODKEY|ShiftMask,    XK_k,                movestack,      {.i = -1 } }, */
  { MODKEY|ShiftMask,    XK_i,                incnmaster,     {.i = +1 } },
  { MODKEY|ShiftMask,    XK_o,                incnmaster,     {.i = -1 } },
  { MODKEY,              XK_h,                setmfact,       {.f = -0.05} },
